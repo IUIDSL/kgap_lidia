96236623a4fd432228edb8c50e61069b86db13b9
littleblackfish <muzcuk@gmail.com>
2021-01-28 23:48:53 -0500

    more refactoring, cleaned up logging

--- app.py
+++ app.py
@@ -12,27 +12,64 @@ with open("neo.json") as f:
     # neo4j_params["auth"] = (neo4j_params.pop("user"), neo4j_params.pop("password"))
     neo4j_params["auth"] = (os.environ["neo4j_user"], os.environ["neo4j_pass"])
 
-###
 with open("drugcentral.json") as f:
     drugcentral_params = json.load(f)
 
-###
+### DB connection helpers
 def Neo4jConnect(params=neo4j_params):
     """Connect to Neo4j db."""
     return neo4j.GraphDatabase.driver(**params).session()
 
-###
 def DrugCentralConnect(params=drugcentral_params):
     """Connect to DrugCentral."""
-
     dbcon = psycopg2.connect(**params)
     dbcon.cursor_factory = psycopg2.extras.DictCursor
     return dbcon
 
-###
-def GetIndication2Drugs(dbcon, indication_query, atc_query=None):
+##  Get unique ATC values
+def GetATCvalues(dbcon):
+    SQL = f"""\
+        SELECT DISTINCT
+            atc.l1_name
+        FROM
+            atc
+        """
+
+    return pd.read_sql(SQL, dbcon).l1_name.to_list()
+
+
+### Globals
+app = Flask(__name__)
+
+### This is the main page
+@app.route("/")
+def landing():
+    return render_template("index.html", atc_values=GetATCvalues(DrugCentralConnect()))
+
+
+### Returns all distinct omop.concept_name values for autocomplete
+@app.route("/indications.json")
+def indications():
+    SQL = "SELECT distinct omop.concept_name FROM omop_relationship omop"
+
+    dbcon = DrugCentralConnect()
+    indications = pd.read_sql(SQL, dbcon).concept_name.to_list()
+
+    return json.dumps(indications)
+
+
+### Returns drugs for a given indication and ATC filter
+@app.route("/drugs.json", methods=["POST"])
+def get_drugs():
     """Query DrugCentral from indication for drugs."""
-    sql = f"""\
+
+    indication_query = request.form["indication"]
+    atc_query = request.form.get("atc", None)
+
+    app.logger.info(f"get_drugs(indication='{indication_query}', atc_query='{atc_query}')")
+
+    dbcon = DrugCentralConnect()
+    SQL = f"""\
     SELECT DISTINCT
         ids.identifier AS pubchem_cid,
         s.id,
@@ -59,59 +96,15 @@ def GetIndication2Drugs(dbcon, indication_query, atc_query=None):
     """
 
     if atc_query:
-        sql += f" AND atc.l1_name ~* %(atc)s "
+        SQL += f" AND atc.l1_name ~* %(atc)s "
         dcdrugs = pd.read_sql(
-            sql, dbcon, params=dict(indication=indication_query, atc=atc_query)
+            SQL, dbcon, params=dict(indication=indication_query, atc=atc_query)
         )
 
     else:
-        dcdrugs = pd.read_sql(sql, dbcon, params=dict(indication=indication_query))
+        dcdrugs = pd.read_sql(SQL, dbcon, params=dict(indication=indication_query))
 
-    logging.debug(f"rows,cols: {dcdrugs.shape[0]},{dcdrugs.shape[1]}")
-    return dcdrugs
-
-
-##  Get unique ATC values
-def GetATCvalues(dbcon):
-    sql = f"""\
-        SELECT DISTINCT
-            atc.l1_name
-        FROM
-            atc
-        """
-
-    return pd.read_sql(sql, dbcon).l1_name.to_list()
-
-### Globals
-app = Flask(__name__)
-
-### This is the main page
-@app.route("/")
-def landing():
-    return render_template("index.html", atc_values=GetATCvalues(DrugCentralConnect()))
-
-### Returns all distinct omop.concept_name values for autocomplete
-@app.route("/indications.json")
-def indications():
-    sql = "SELECT distinct omop.concept_name FROM omop_relationship omop"
-
-    dbcon = DrugCentralConnect()
-    indications = pd.read_sql(sql, dbcon).concept_name.to_list()
-
-    return json.dumps(indications)
-
-### Returns drugs for a given indication and ATC filter
-@app.route("/drugs.json", methods=["POST"])
-def get_drugs():
-
-    indication_query = request.form["indication"]
-    atc_query = request.form.get("atc", None)
-
-    print(indication_query, atc_query)
-
-    dbcon = DrugCentralConnect()
-
-    dcdrugs = GetIndication2Drugs(dbcon, indication_query, atc_query=atc_query)
+    app.logger.debug(f"rows,cols: {dcdrugs.shape[0]},{dcdrugs.shape[1]}")
 
     buffer = dict(
         disease_list=list(dcdrugs["omop_concept_name"].unique()),
@@ -128,8 +121,8 @@ def get_drugs():
 def get_genes():
 
     cid_list = json.loads(request.form["cid_list"])
-
-    print(type(cid_list), cid_list)
+    
+    app.logger.info(f"get_genes(cid_list='{cid_list}')")
 
     # score_attribute = "sum(s.degree)"
     score_attribute = "sum(r.zscore)/sqrt(count(r))"
@@ -141,13 +134,11 @@ def get_genes():
     RETURN g.id as ncbiGeneId, g.name as geneSymbol, g.tdl as TDL, score as kgapScore
     ORDER BY score DESC
     """
-    #app.logger.info(f"CQL: {CQL}")
+    # app.logger.info(f"CQL: {CQL}")
 
     session = Neo4jConnect()
 
-    data = session.run(
-        CQL, parameters=dict(cid_list=cid_list)
-    ).data()
+    data = session.run(CQL, parameters=dict(cid_list=cid_list)).data()
 
     cdf = pd.DataFrame(data)
 
@@ -166,8 +157,8 @@ def get_evidence_path():
     gene = request.form["gene"]
 
     cid_list = json.loads(request.form["cid_list"])
-
-    print(len(cid_list), cid_list)
+    
+    app.logger.info(f"get_evidence_path(gene='{gene}', cid_list='{cid_list}')")
 
     CQL = f"MATCH p=(d:Drug)-[]-(s:Signature)-[sg]-(g:Gene {{name: $gene_name }}) WHERE d.pubchem_cid in $cid_list RETURN d,g"
 
@@ -197,7 +188,7 @@ def get_evidence_path():
 
     response = nx.readwrite.json_graph.cytoscape.cytoscape_data(graph)
 
-    #with open(f"tmp/{gene}_evidence_path.json", "w") as f:
+    # with open(f"tmp/{gene}_evidence_path.json", "w") as f:
     #    json.dump(response, f)
 
     return json.dumps(response)
