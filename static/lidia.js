227b5db4e37e9270fa2a353c8d95f9a2feca0453
littleblackfish <muzcuk@gmail.com>
2021-02-02 23:09:49 -0500

    some visual improvements

--- static/lidia.js
+++ static/lidia.js
@@ -12,19 +12,31 @@ var chosen_gene, cy_evidence_path;
 
 $(document).ready(fetch('indications.json')
     .then(response => response.json())
-    .then(data => $("#indication-input").autocomplete({ source: data })))
+    .then(data => $("#indication-input").autocomplete({
+        source: data
+    })))
 
 function about() {
-    $('#about-button').fadeOut(100, function () { $('#about-text').fadeIn('slow') })
+    $('#about-button').fadeOut(100, function() {
+        $('#about-text').fadeIn('slow')
+    })
 }
 
 function start() {
-    $('#introduction').fadeOut('slow', function () { $('#input').fadeIn('slow') });
+    $('#introduction').fadeOut('slow', function() {
+        $('#input').fadeIn('slow')
+    });
 }
 
 function output() {
-    $('#input').fadeOut('slow', function () { $('#output').fadeIn('slow') });
+    $('#input').fadeOut('slow', function() {
+        $('#output').fadeIn('slow', function() {
+            renderEvidenceBackbone(50, 150);
+            gene_table.draw();
+        })
+    });
 }
+
 
 function processDrugs(data) {
     // retrieved diseases
@@ -38,8 +50,6 @@ function processDrugs(data) {
     for (drug of drug_list) {
         cid_list.push(drug.id)
     }
-
-    $('#output-text').append('<p>' + disease_list + '</p>')
 
     return cid_list
 }
@@ -57,22 +67,41 @@ function run() {
     //formData.append('indication', indication);
     //formData.append('atc', atc);
 
-    fetch('drugs.json', { body: formData, method: "post" })
+    fetch('drugs.json', {
+            body: formData,
+            method: "post"
+        })
         .then(response => response.json())
         .then(data => processDrugs(data))
         .then(cid_list => getGenes(cid_list))
-        .then(data => output(data))
 
     return false
 }
 
 function processGenes(data) {
 
-    let columns = [
-        { data: 'geneSymbol', title: 'Symbol', searchable: true, orderable: false },
-        { data: 'TDL', title: 'TDL', orderable: false },
-        { data: 'sign', title: 'sign', orderable: true },
-        { data: 'absScore', title: 'score', searchable: false, orderable: true }
+    let columns = [{
+            data: 'geneSymbol',
+            title: 'Symbol',
+            searchable: true,
+            orderable: false
+        },
+        {
+            data: 'TDL',
+            title: 'TDL',
+            orderable: false
+        },
+        {
+            data: 'sign',
+            title: 'sign',
+            orderable: true
+        },
+        {
+            data: 'absScore',
+            title: 'score',
+            searchable: false,
+            orderable: true
+        }
     ]
 
     gene_list = data;
@@ -86,14 +115,16 @@ function processGenes(data) {
         paging: false,
         scrollCollapse: true,
         scrollY: "400px",
-        order: [[3, 'desc']],
-        initComplete: function () {
+        order: [
+            [3, 'desc']
+        ],
+        initComplete: function() {
 
             var column = this.api().column(0)
 
             $(column.header()).empty().append('<input type="text" class="u-full-width" placeholder="Gene symbol">')
 
-            $('input', column.header()).on('keyup change clear', function () {
+            $('input', column.header()).on('keyup change clear', function() {
                 gene_table.column(0).search(this.value).draw();
 
             });
@@ -102,7 +133,7 @@ function processGenes(data) {
             column = this.api().column(1)
             var select = $('<select><option value="">TDL</option></select>')
                 .appendTo($(column.header()).empty())
-                .on('change', function () {
+                .on('change', function() {
                     var val = $.fn.dataTable.util.escapeRegex(
                         $(this).val()
                     );
@@ -112,7 +143,7 @@ function processGenes(data) {
                         .draw();
                 });
 
-            column.data().unique().sort().each(function (d, j) {
+            column.data().unique().sort().each(function(d, j) {
                 select.append('<option value="' + d + '">' + d + '</option>')
             });
             this.api().draw();
@@ -121,12 +152,11 @@ function processGenes(data) {
     });
 
 
-    $('#gene-table').on('click', 'tr', function () {
+    $('#gene-table').on('click', 'tr', function() {
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
             chosen_gene = undefined
-        }
-        else {
+        } else {
             gene_table.$('tr.selected').removeClass('selected');
             $(this).addClass('selected');
             chosen_gene = gene_table.row('.selected').data().geneSymbol
@@ -141,59 +171,73 @@ function processGenes(data) {
 function getGenes() {
     if (cid_list.length == 0) {
         console.log('no drugs came up for the query')
-        return undefined 
+        return undefined
     }
 
     let formData = new FormData();
     formData.append('cid_list', JSON.stringify(cid_list.map(Number)));
 
 
-    fetch('genes.json', { body: formData, method: "post" })
+    fetch('genes.json', {
+            body: formData,
+            method: "post"
+        })
         .then(response => response.json())
         .then(data => processGenes(data))
-        .then(data=> renderEvidenceBackbone(50,150));
+        .then(summary_text)
+        .then(output);
 }
 
 function renderEvidenceBackbone(min_diameter, max_diameter) {
 
     s = [{
-        "selector": "node",
-        "style": {
-            "label": "data(name)",
-            "font-size": 36,
-            "width": "data(diameter)",
-            "height": "data(diameter)",
-            "text-valign": "center",
-            "text-halign": "center"
-        }
-    },
-    {
-        "selector": "edge",
-        "style": {
-            //   "width": "data(weight)",
-            "width": 2,
-            //   "curve-style": "haystack",
-            //   "haystack-radius": 0.5
-            "curve-style": "bezier",
-            "control-point-step-size": 5
+            "selector": "node",
+            "style": {
+                "label": "data(name)",
+                "font-size": 36,
+                "width": "data(diameter)",
+                "height": "data(diameter)",
+                "text-valign": "center",
+                "text-halign": "center"
+            }
         },
-    }]
+        {
+            "selector": "edge",
+            "style": {
+                //   "width": "data(weight)",
+                "width": 2,
+                //   "curve-style": "haystack",
+                //   "haystack-radius": 0.5
+                "curve-style": "bezier",
+                "control-point-step-size": 5
+            },
+        }
+    ]
 
     nodes = [];
     for (drug of drug_list) {
         drug['level'] = 1;
         drug['diameter'] = min_diameter + (drug.gene_scale * (max_diameter - min_diameter));
-        nodes.push({ data: drug });
+        nodes.push({
+            data: drug
+        });
     }
 
     cy_evidence_path = cytoscape({
         container: document.getElementById('cy'),
         layout: {
-            name: 'concentric', concentric: function (node) { return node.data('level'); },
+            name: 'concentric',
+            concentric: function(node) {
+                return node.data('level');
+            },
             minNodeSpacing: 100,
-            levelWidth: function (nodes) { return 1; }
+            levelWidth: function(nodes) {
+                return 1;
+            }
         },
-        elements: { nodes: nodes },
+        elements: {
+            nodes: nodes
+        },
         style: s
     })
 }
@@ -210,36 +254,62 @@ function renderEvidenceEdges(gene) {
     // add center node
     cy_evidence_path.add({
         group: 'nodes',
-        data: { id: gene, diameter: 100, name: gene, level: 2},
+        data: {
+            id: gene,
+            diameter: 100,
+            name: gene,
+            level: 2
+        },
         class: 'gene',
-        position: { x: 250, y: 250 }
+        position: {
+            x: 250,
+            y: 250
+        }
     })
 
-    fetch('edges.json', { body: formData, method: "post" })
+    fetch('edges.json', {
+            body: formData,
+            method: "post"
+        })
         .then(response => response.json())
-        .then(edges => cy_evidence_path.add({ edges: edges }));
+        .then(edges => cy_evidence_path.add({
+            edges: edges
+        }));
 }
 
 
+function summary_text() {
 
-function getEvidencePath(gene) {
+    let summary = `<p> Query <strong>${indication}</strong> `
 
-    let formData = new FormData();
-    // formData.append('indication', indication);
-    // formData.append('atc', atc);
-    formData.append('gene', gene);
-    formData.append('cid_list', JSON.stringify(cid_list.map(Number)));
+    if (atc) {
+        summary += `with ATC filter <strong>${atc}</strong> `
+    }
+    summary += 'matched <span class="tooltip"> <strong>' +
+        disease_list.length +
+        '</strong> OMAP concepts <span class="tooltiptext">';
+    for (disease of disease_list) {
+        summary += disease + '<br/>';
+    }
+    summary += '</span> </span> <br/>' +
+        'which yielded <span class="tooltip"><strong>' +
+        drug_list.length +
+        '</strong> drugs <span class="tooltiptext">';
+    for (drug of drug_list) {
+        summary += drug.name + '<br/>';
+    }
+    summary += '</span> </span> and <strong>' +
+        gene_list.length +
+        '</strong> potential targets. </p>'
 
-    fetch('evidence_path.json', { body: formData, method: "post" })
-        .then(response => response.json())
-        .then(data => renderEvidencePath(data));
+    $('#output-text').append(summary)
 }
 
 function reset() {
-    
+    $('#output').fadeOut('slow', function() {
+        $('#input').fadeIn('slow')
+    });
 
-    $('#output').fadeOut('slow', function() {$('#input').fadeIn('slow')});
-    
     // reset global variables
     indication = undefined;
     atc = undefined;
@@ -254,11 +324,9 @@ function reset() {
     try {
         gene_table.destroy();
         cy_evidence_path.destroy();
-    }
-    finally {
+    } finally {
         $('#output-text').empty();
         $('#gene-table').empty();
         $('#input-form')[0].reset();
-
     }
 }
